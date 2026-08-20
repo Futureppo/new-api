ef5e329d76fea2c5683afe17a08b5dc4cc2d2dba
Futureppo <futureppo666@gmail.com>
2026-08-20 16:14:26 +0800

    feat: 所有分组的描述均可编辑

--- controller/option.go
+++ controller/option.go
@@ -217,6 +217,15 @@ func UpdateOption(c *gin.Context) {
 			})
 			return
 		}
+	case "GroupDescriptions":
+		err = setting.CheckGroupDescriptions(option.Value.(string))
+		if err != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"success": false,
+				"message": err.Error(),
+			})
+			return
+		}
 	case "group_ratio_setting.group_display":
 		err = ratio_setting.CheckGroupDisplay(option.Value.(string))
 		if err != nil {
